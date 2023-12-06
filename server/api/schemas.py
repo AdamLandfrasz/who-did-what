a76f3db48b7b92b064fe53ff52e807888334c085
Adam Landfrasz <adam.landfrasz@lensa.com>
2023-12-06 14:41:26 +0100

    create initial player schema

--- server/api/schemas.py
+++ server/api/schemas.py
@@ -1,5 +1,16 @@
-from pydantic import BaseModel
+from fastapi import WebSocket
+from pydantic import BaseModel, ConfigDict
 
 
 class SuccessResponse(BaseModel):
     success: bool = True
+
+
+class Player(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    session_id: str
+    websocket: WebSocket
+    joined: bool = False
+    name: str | None = None
+    room: str | None = None
